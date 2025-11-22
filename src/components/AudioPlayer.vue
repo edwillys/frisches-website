bd06090f3e9047b62d3cc0799637ab4cf6b4b5dc
Edgar Lubicz <edgarlubicz@gmail.com>
2025-11-23 00:31:40 +0100

    feat: Add mute functionality to AudioPlayer component with volume state management

--- src/components/AudioPlayer.vue
+++ src/components/AudioPlayer.vue
@@ -29,6 +29,7 @@ const isShuffle = ref(false)
 const repeatMode = ref<'off' | 'all' | 'one'>('off')
 const isPlaylistOpen = ref(false)
 const volume = ref(1)
+const previousVolume = ref(1)
 const showVolume = ref(false)
 const audioRef = ref<HTMLAudioElement | null>(null)
 
@@ -200,6 +201,15 @@ const onEnded = () => {
   }
 }
 
+const toggleMute = () => {
+  if (volume.value > 0) {
+    previousVolume.value = volume.value
+    volume.value = 0
+  } else {
+    volume.value = previousVolume.value || 1
+  }
+}
+
 // Watchers
 watch(currentTrackIndex, () => {
   if (audioRef.value) {
@@ -292,7 +302,7 @@ watch(volume, (newVol) => {
             @mouseenter="showVolume = true" 
             @mouseleave="showVolume = false"
           >
-            <button class="btn-icon small">
+            <button class="btn-icon small" @click="toggleMute">
               <svg v-if="volume > 0.5" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"></polygon><path d="M19.07 4.93a10 10 0 0 1 0 14.14M15.54 8.46a5 5 0 0 1 0 7.07"></path></svg>
               <svg v-else-if="volume > 0" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"></polygon><path d="M15.54 8.46a5 5 0 0 1 0 7.07"></path></svg>
               <svg v-else xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"></polygon><line x1="23" y1="9" x2="17" y2="15"></line><line x1="17" y1="9" x2="23" y2="15"></line></svg>
