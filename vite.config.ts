dc1ce06b3181a2216964fd020a66f25db7f8415a
Edgar Lubicz <edgarlubicz@gmail.com>
2025-12-23 13:54:01 +0100

    feat: Update Vite configuration to use templateCompilerOptions in Vue plugin

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { templateCompilerOptions } from '@tresjs/core'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue(templateCompilerOptions),
     vueJsx(),
     vueDevTools(),
   ],
