0722ca3d9310f135f2c39211a9f72567bbbc9e57
Edgar Lubicz <edgarlubicz@gmail.com>
2025-12-28 15:51:55 +0100

    fix: format globalIgnores for better readability in eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -16,16 +16,21 @@ export default defineConfigWithVueTs(
     files: ['**/*.{ts,mts,tsx,vue}'],
   },
 
-  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+  globalIgnores([
+    '**/public/draco',
+    '**/dist/**',
+    '**/dist-ssr/**',
+    '**/coverage/**'
+  ]),
 
   pluginVue.configs['flat/essential'],
   vueTsConfigs.recommended,
-  
+
   {
     ...pluginVitest.configs.recommended,
     files: ['src/**/__tests__/*'],
   },
-  
+
   {
     ...pluginPlaywright.configs['flat/recommended'],
     files: ['e2e/**/*.{test,spec}.{js,ts,jsx,tsx}'],
